c9391ea1340c3593503e254366dc1b8894a61b10
Matthew Etchells <matthewetchells@Matthews-MBP.local>
2018-01-09 04:50:31 -0500

    csv read/write and class files.

---
+++ groc.py
@@ -0,0 +1,6 @@
+class groc:
+    def __init__(self, name, price, mass):
+        self.name = name
+        self.price = price
+        self.mass = mass
+
---
+++ receipt.py
@@ -0,0 +1,10 @@
+class receipt:
+    def __init__(self):
+        self.groc_list = {}
+
+    def add_groc(self, groc):
+        groc_list[groc.name] = (groc.mass, groc.price)
+
+
+
+
